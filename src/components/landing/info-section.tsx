34acbe6e361e9b3984fd803677e8f9a7c0d66558
Matheus Barros <matheusbg21@gmail.com>
2025-08-16 19:12:19 +0000

    coloque mais essa imagem na seção: https://i.imgur.com/FC328r1.png
    uma

--- src/components/landing/info-section.tsx
+++ src/components/landing/info-section.tsx
@@ -4,7 +4,7 @@ export default function InfoSection() {
   return (
     <section className="bg-accent py-16 sm:py-24">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 items-center gap-8 md:grid-cols-2">
+        <div className="grid grid-cols-1 items-center gap-8 md:grid-cols-3">
           <div className="flex justify-center overflow-hidden rounded-2xl shadow-lg">
             <Image
               src="https://i.imgur.com/oMt2kcj.png"
@@ -18,6 +18,15 @@ export default function InfoSection() {
             <Image
               src="https://i.imgur.com/SsiDE98.png"
               alt="Um novo capítulo na história de Pirenópolis"
+              width={550}
+              height={384}
+              className="h-auto w-full max-w-full rounded-2xl"
+            />
+          </div>
+          <div className="flex justify-center overflow-hidden rounded-2xl shadow-lg">
+            <Image
+              src="https://i.imgur.com/FC328r1.png"
+              alt="Vista para o Morro do Frota"
               width={550}
               height={384}
               className="h-auto w-full max-w-full rounded-2xl"
