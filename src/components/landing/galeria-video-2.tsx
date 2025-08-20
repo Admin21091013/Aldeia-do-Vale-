f904d404a70cd72bc09a95095bde3817cf7d7f98
Matheus Barros <matheusbg21@gmail.com>
2025-08-20 14:15:26 +0000

    substitua um dos vídeos pra esse:
    <script src="https://fast.wistia.com

--- src/components/landing/galeria-video-2.tsx
+++ src/components/landing/galeria-video-2.tsx
@@ -16,7 +16,7 @@ export function GaleriaVideo2() {
   useEffect(() => {
     window._wq = window._wq || [];
     window._wq.push({
-      id: "aa25uqsskd",
+      id: "kqwhn0t2u8",
       onReady: (video: any) => {
         console.log("Wistia video ready:", video);
       }
@@ -26,11 +26,11 @@ export function GaleriaVideo2() {
   return (
     <div className="w-full">
         <Script src="https://fast.wistia.com/assets/external/E-v1.js" async />
-        <div className="wistia_responsive_padding" style={{padding: '177.78% 0 0 0', position: 'relative'}}>
+        <div className="wistia_responsive_padding" style={{padding: '176.39% 0 0 0', position: 'relative'}}>
             <div className="wistia_responsive_wrapper" style={{height: '100%', left: 0, position: 'absolute', top: 0, width: '100%'}}>
             <div 
                 ref={videoRef}
-                className="wistia_embed wistia_async_aa25uqsskd videoFoam=true" 
+                className="wistia_embed wistia_async_kqwhn0t2u8 videoFoam=true" 
                 style={{height: '100%', position: 'relative', width: '100%', borderRadius: '1rem', overflow: 'hidden'}}
             >
                 <div 
@@ -38,7 +38,7 @@ export function GaleriaVideo2() {
                 style={{height: '100%', left: 0, opacity: 0, overflow: 'hidden', position: 'absolute', top: 0, transition: 'opacity 200ms', width: '100%'}}
                 >
                 <img 
-                    src="https://fast.wistia.com/embed/medias/aa25uqsskd/swatch" 
+                    src="https://fast.wistia.com/embed/medias/kqwhn0t2u8/swatch" 
                     style={{filter: 'blur(5px)', height: '100%', objectFit: 'contain', width: '100%'}} 
                     alt="" 
                     aria-hidden="true" 
