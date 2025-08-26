bef069772c73f94adcad9865b0dbda110bd765af
Matheus Barros <matheusbg21@gmail.com>
2025-08-26 23:54:45 +0000

    Viver no Aldeia do Vale. precisa estar junto na primeira linha da headli

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -28,8 +28,7 @@ export default function Hero({ onSuccessfulSubmit, onIndicationClick }: HeroProp
           <div className="text-center text-white lg:text-left">
             <h1 className="font-headline text-4xl leading-tight text-white md:text-5xl lg:text-6xl">
               Viver no Aldeia do Vale <br />
-              é um privilégio <br />
-              que a natureza <br />
+              é um privilégio que a natureza <br />
               concede a poucos.
             </h1>
             <p className="mx-auto mt-6 max-w-2xl text-lg text-sand-200 md:text-xl lg:mx-0">
